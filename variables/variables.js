6ff526c1c41ca0a9aca219cd8e8679fea3e246f5
Kodirov2samir <kodirovsamir174@gmail.com>
2025-09-06 22:44:09 +0500

    variables

---
+++ variables/variables.js
@@ -0,0 +1,10 @@
+// //////////////////////////////////Variables///////////////////////////////////////////////////////
+// const 1win = "hello" => error because we cant name a variable with a number in the beginning
+
+// Dynamic typing => means that i dont need to write whether the variable contains string,number,boolean etc
+let contains; 
+console.log(console.log(contains)); //=>the result is undefined
+
+// const contains; 
+// console.log(console.log(contains)); => the result is an error
+// Use const when you can, and use let when you have to.
